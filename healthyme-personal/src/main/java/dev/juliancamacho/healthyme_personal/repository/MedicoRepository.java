6c34be2daa111b1c62d7637fec358609ee9b72b9
TheJose <devbyjose@gmail.com>
2025-07-20 15:10:40 -0500

    fix(payment): metodo erroneo jpa
    - se corrige metodo errone de jpa en MedicoRepository

--- healthyme-personal/src/main/java/dev/juliancamacho/healthyme_personal/repository/MedicoRepository.java
+++ healthyme-personal/src/main/java/dev/juliancamacho/healthyme_personal/repository/MedicoRepository.java
@@ -7,6 +7,4 @@ import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<Medico, Integer> {
     List<Medico> findByEspecialidadIdEspecialidad(Integer idEspecialidad);
-
-    Long countByEstadoTrue(); // Contar médicos activos
 }

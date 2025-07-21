6f4f911c6ebd5a8d2c4de74983a1cc4a2908d805
TheJose <devbyjose@gmail.com>
2025-07-20 23:20:04 -0500

    fix(security): endpoint para informacino del usuario en sesion
    - se corrige endpoint para  obtner la infromacino del usuario en sesion

--- security-service/src/main/java/studio/devbyjose/security_service/config/SecurityConfig.java
+++ security-service/src/main/java/studio/devbyjose/security_service/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/users/me").authenticated()
                         .requestMatchers("/api/v1/auth/**", "/api/v1/users/**","/oauth2/**", "/login").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Para pre-flight CORS requests
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/actuator/**").permitAll()
